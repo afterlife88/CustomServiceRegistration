c909cd9fb1e8bf050e7d20a758a7157b3590871c
Andrew Iasko <andrii.iasko@gmail.com>
2016-09-15 19:05:07 +0300

    Added connection string and init user table

--- src/CustomServiceRegistration.Domain/Context/DataDbContext.cs
+++ src/CustomServiceRegistration.Domain/Context/DataDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using CustomServiceRegistration.Domain.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace CustomServiceRegistration.Domain.Context
 {
@@ -8,11 +9,8 @@ namespace CustomServiceRegistration.Domain.Context
 	{
 		public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
 		{
-
+			Database.EnsureCreated();
 		}
-		protected override void OnModelCreating(ModelBuilder builder)
-		{
-			base.OnModelCreating(builder);
-		}
+		
 	}
 }
--- src/CustomServiceRegistration/Startup.cs
+++ src/CustomServiceRegistration/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CustomServiceRegistration.Domain.Context;
+using CustomServiceRegistration.Domain.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -12,6 +16,14 @@ namespace CustomServiceRegistration
 	{
 		public void ConfigureServices(IServiceCollection services)
 		{
+			var connection =
+				@"Server=tcp:itcompetitions.database.windows.net,1433;Database=devchallenge;User ID=admin@yo.com@itcompetitions;Password=nLcJZ8i9;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+			services.AddDbContext<DataDbContext>(options => options.UseSqlServer(connection));
+
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<DataDbContext>()
+				.AddDefaultTokenProviders();
+
 			services.AddMvc().AddJsonOptions(options =>
 			{
 				options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
@@ -34,7 +46,8 @@ namespace CustomServiceRegistration
 
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
-			
+			//app.UseIdentity();
+
 			app.UseMvc(routes =>
 			{
 				routes.MapWebApiRoute(
@@ -47,8 +60,28 @@ namespace CustomServiceRegistration
 			});
 			app.UseSwagger();
 			app.UseSwaggerUi();
-		
+
+			//CreateSampleData(app.ApplicationServices);
 
 		}
+
+		//private static async void CreateSampleData(IServiceProvider applicationServices)
+		//{
+		//	using (var dbContext = applicationServices.GetService<DataDbContext>())
+		//	{
+		//		var sqlServerDatabase = dbContext.Database;
+		//		if (sqlServerDatabase != null)
+		//		{
+		//			// add some users
+		//			var userManager = applicationServices.GetService<UserManager<ApplicationUser>>();
+		//			ApplicationUser user = await userManager.FindByEmailAsync("test01@example.com");
+		//			if (user == null)
+		//			{
+		//				user = new ApplicationUser {UserName = "test01", Email = "test01@example.com"};
+		//				await userManager.CreateAsync(user, "Qwer!@#12345");
+		//			}
+		//		}
+		//	}
+		//}
 	}
 }
